af99c4e6a18c1f16311571c8d28619f1481d06e3
Muzakki839 <muzakki839@gmail.com>
2025-01-11 03:18:38 +0700

    feat: Integrate Firebase authentication and initialize Firebase in main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:minichat/firebase_options.dart';
 import 'package:minichat/pages/auth/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
