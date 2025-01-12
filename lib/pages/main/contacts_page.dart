f16a5b6b6048ee91de0bf7a360ca88b4a94f0025
Muzakki839 <muzakki839@gmail.com>
2025-01-12 17:11:50 +0700

    feat: Create ContactsPage layout structure with contact list and add contact button

--- lib/pages/main/contacts_page.dart
+++ lib/pages/main/contacts_page.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:minichat/pages/main_page.dart';
+import 'package:minichat/widgets/item/user_tile.dart';
 
 class ContactsPage extends StatelessWidget {
   const ContactsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          // build contact list
+          Expanded(child: _buildContactList()),
+
+          // add contact button
+          Padding(
+            padding: const EdgeInsets.only(bottom: 120, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // TODO: create add_contact_page.dart
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                    );
+                  },
+                  backgroundColor: theme.onPrimary,
+                  foregroundColor: theme.primary,
+                  child: Icon(Icons.person_add_rounded),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContactList() {
+    // TODO: load contact list using streambuilder from hive box
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 0, left: 5, right: 5),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return _buildContactItem();
+        },
+        itemCount: 30,
+      ),
+    );
+  }
+
+  Widget _buildContactItem() {
+    // TODO: load individual contact data
+    return UserTile(
+      text: "data",
+      onTap: () {},
+    );
   }
 }
